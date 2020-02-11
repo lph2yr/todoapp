a18eabebbe1608ee76134e9c60ecc7fae9713abd
Abby Morgan <abby@northerncrown.com>
2020-02-10 20:50:13 -0500

    making models.py have some optional fields

--- todo/models.py
+++ todo/models.py
@@ -7,10 +7,10 @@ from django.utils import timezone
 
 class ToDoItem(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=600, default="")
+    description = models.CharField(max_length=600, blank=True, default="")
     duedate = models.DateTimeField()
     #date_added = models.DateTimeField()
-    location = models.CharField(max_length=50)
+    location = models.CharField(max_length=50, blank=True)
     completed = models.BooleanField(default=False) 
     def __str__(self):
     	return self.title
