a6bde38a96a56746f622e7b6fc2f3e4596c71764
Abby Morgan <abby@northerncrown.com>
2020-02-10 20:35:30 -0500

    edited some ui elements

--- personaldashboard/settings.py
+++ personaldashboard/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
     'todo.apps.TodoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
