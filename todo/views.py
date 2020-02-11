5d3077ee4ebf5674c4150c6683ce8793150a7b92
Abby Morgan <abby@northerncrown.com>
2020-02-10 19:48:21 -0500

    Made /todo_list work!

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import ToDoItem
 
@@ -10,4 +11,4 @@ class ToDoListView(generic.ListView):
 
     #This is how the tasks are gathered!
     def get_queryset(self):
-        return ToDoItem.objects().order_by(date)
+        return ToDoItem.objects#.order_by(date)
