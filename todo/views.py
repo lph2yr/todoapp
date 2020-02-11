96d734e94f44fb3d645ed16f0afe3350f6712bb4
Abby Morgan <abby@northerncrown.com>
2020-02-10 20:10:54 -0500

    Made base to-do list page

--- todo/views.py
+++ todo/views.py
@@ -6,9 +6,11 @@ from .models import ToDoItem
 # Create your views here.
 
 class ToDoListView(generic.ListView):
+    #the template this view uses
     template_name = 'todo/todo_list.html'
+    #this is what the list with the todo items is called
     context_object_name = 'todo_list'
 
     #This is how the tasks are gathered!
     def get_queryset(self):
-        return ToDoItem.objects#.order_by(date)
+        return ToDoItem.objects.order_by('date')
