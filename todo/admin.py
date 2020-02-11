d3d520be601ab5115c32cc6115689d96e624803b
Ann Hoang <lph2yr@virginia.edu>
2020-02-11 02:06:16 -0500

    admin more details

--- todo/admin.py
+++ todo/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import ToDoItem
 
-admin.site.register(ToDoItem)
+class ToDoAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['title']}),
+        ('Details', {'fields': [ 'description', 'duedate', 'location', 'completed' ] }),
+    ]
+
+    list_display = ('title', 'duedate', 'completed')
+    list_filter = ['duedate']
+    search_fields = ['title']
+
+admin.site.register(ToDoItem, ToDoAdmin )
