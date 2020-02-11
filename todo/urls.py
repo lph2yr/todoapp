20b126d840b32b2c51f8502065146be8a655752d
Abby Morgan <abby@northerncrown.com>
2020-02-10 19:31:05 -0500

    adding template lorem ipsum, adding url

---
+++ todo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'todo_list'
+urlpatterns = [
+    path('', views.ToDoListView.as_view(), name='todo_list'),
+
+]
--- todo/views.py
+++ todo/views.py
@@ -5,7 +5,9 @@ from .models import ToDoItem
 # Create your views here.
 
 class ToDoListView(generic.ListView):
-    template_name = 'todo/index.html'
+    template_name = 'todo/todo_list.html'
     context_object_name = 'todo_list'
 
     #This is how the tasks are gathered!
+    def get_queryset(self):
+        return ToDoItem.objects().order_by(date)
