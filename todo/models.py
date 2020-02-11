26f7bb8394b14281307b021f7de22ae8b33272e8
Abby Morgan <abby@northerncrown.com>
2020-02-10 19:15:58 -0500

    added views.py, changed ToDoList to ToDoItem

--- todo/models.py
+++ todo/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 # Create your models here.
 
-class toDoList(models.Model):
+class toDoItem(models.Model):
     title = models.CharField(max_length=100)
     time = models.DateTimeField()
     date = models.DateField()
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import ToDoItem
+
 # Create your views here.
+
+class ToDoListView(generic.ListView):
+    template_name = 'todo/index.html'
+    context_object_name = 'todo_list'
+
+    #This is how the tasks are gathered!
