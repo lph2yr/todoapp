c7deb80d540696ed90b508fbcc419d07fe765353
aaron-gu <ag5ym@virginia.edu>
2020-02-13 12:00:48 -0800

    add completed tasks page

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'todo_list'
 urlpatterns = [
     path('', views.ToDoListView.as_view(), name='todo_list'),
-
+    path('completed/', views.CompletedView.as_view(), name='completed'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from datetime import datetime
 
 from .models import ToDoItem
 
@@ -13,4 +14,11 @@ class ToDoListView(generic.ListView):
 
     #This is how the tasks are gathered!
     def get_queryset(self):
-        return ToDoItem.objects.order_by('duedate')
+        return ToDoItem.objects.filter(completed=False).order_by('duedate')
+
+class CompletedView(generic.ListView):
+    template_name = 'todo/todo_list.html'
+    context_object_name = 'todo_list'
+    def get_queryset(self):
+        return ToDoItem.objects.filter(completed=True).order_by('duedate')
+
