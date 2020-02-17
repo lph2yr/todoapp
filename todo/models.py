cc99a27ab37f4ec8ff426921f2075f140cea067f
Ann Hoang <lph2yr@virginia.edu>
2020-02-17 16:30:52 -0500

    add some recurring fields

--- todo/models.py
+++ todo/models.py
@@ -11,8 +11,32 @@ class ToDoItem(models.Model):
     duedate = models.DateTimeField()
     #date_added = models.DateTimeField()
     location = models.CharField(max_length=50, blank=True)
-    completed = models.BooleanField(default=False) 
+    completed = models.BooleanField(default=False)
+    recur_freq = models.TextField(max_length=7)
+        #daily, weekly, monthly, yearly
+    end_recur_date = models.DateTimeField()
+    #end repeat date and time
+
     def __str__(self):
     	return self.title
 
+    def createToDo(self, recur_date, false_completed, freq, end_date ):
+        # creating new To Do items with same title, description, location
+        recur_todo = self( title = self.title,
+                           description = self.description,
+                           location = self.location,
+                           duedate = recur_date,
+                           completed = false_completed,
+                           recur_freq = freq,
+                           end_recur_date = end_date )
+        #changing due date; default false_completed
+        return recur_todo
+'''
+ALGORITHM:
+User -> edit 
+Recur -> list drop down
+    end_date appears --> if don't provide end_date --> error message
+    if provide end_date
+    
+'''
     
