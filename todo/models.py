f51d5541a76638fdec9efb7de52c5e12bf31a9ea
Ann Hoang <lph2yr@virginia.edu>
2020-02-17 16:41:21 -0500

    Added 2 fields

--- todo/models.py
+++ todo/models.py
@@ -14,29 +14,40 @@ class ToDoItem(models.Model):
     completed = models.BooleanField(default=False)
     recur_freq = models.TextField(max_length=7)
         #daily, weekly, monthly, yearly
+        #customize day of week
+        #every other day
+        #every other week
+
     end_recur_date = models.DateTimeField()
     #end repeat date and time
+    #end after a day
+    #never
+    #end after # occurrences
 
     def __str__(self):
     	return self.title
 
-    def createToDo(self, recur_date, false_completed, freq, end_date ):
+    def createToDo(self, freq, recur_date, end_date ):
         # creating new To Do items with same title, description, location
+        # changing due date; default false_completed
         recur_todo = self( title = self.title,
                            description = self.description,
                            location = self.location,
                            duedate = recur_date,
-                           completed = false_completed,
+                           completed = False,
                            recur_freq = freq,
                            end_recur_date = end_date )
-        #changing due date; default false_completed
+        #recur date = current due date + (daily = +1) or (weekly = + 7) or (monthly = ...) or (yearly = ....)
         return recur_todo
 '''
 ALGORITHM:
 User -> edit 
-Recur -> list drop down
+Recur_freq -> list drop down --> choose --> update recur_freq field
     end_date appears --> if don't provide end_date --> error message
-    if provide end_date
-    
+    if provide end_date --> change end_recur_date field
+        #never --> have it end in 2500
+        #after date --> change end_recur_date field
+        #after # occurrences --> count occurences
+    create object
 '''
     
