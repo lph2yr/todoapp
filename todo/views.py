0c4f8af0ca9816b0bdfc2cb6d957f1eb55d21643
Ann Hoang <lph2yr@virginia.edu>
2020-02-18 14:16:51 -0500

    updateview attempt

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 from .forms import ToDoForm
 from .models import ToDoItem
-
+from django.views.generic.edit import UpdateView
 
 # Create your views here.
 class ToDoListView(generic.ListView):
@@ -15,6 +15,13 @@ class ToDoListView(generic.ListView):
     def get_queryset(self):
         return ToDoItem.objects.filter(completed=False).order_by('-duedate')
 
+class ToDoUpdate( UpdateView ):
+    model = ToDoItem
+    fields = ['duedate'] #only dudate for now
+    template_name_suffix = '_update_form'
+
+#https://docs.djangoproject.com/en/3.0/topics/class-based-views/generic-editing/
+'''    
 #function processes input data of Date and Time and updates it in Database for todo_item at todo_item_id
 #@param: request, todo_item_id
 def detail( request, todo_item_id ):
@@ -30,7 +37,7 @@ def detail( request, todo_item_id ):
         form = ToDoForm() #reset blank form
     context = {'todo_item': todo_item, 'form':form}
     return render(request, 'todo/detail.html', context)
-
+'''
 #function changes a todo from incomplete to complete (completed = False -> True)
 def completeToDo(request, todo_item_id):
     #Todo item to be completed
