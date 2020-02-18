33cc56afad58af3fe6cf8052c12bb34e97c8becc
Ann Hoang <lph2yr@virginia.edu>
2020-02-17 19:07:34 -0500

    added priority to admin

--- todo/admin.py
+++ todo/admin.py
@@ -6,10 +6,10 @@ from .models import ToDoItem
 class ToDoAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['title']}),
-        ('Details', {'fields': [ 'description', 'duedate', 'location', 'completed' ] }),
+        ('Details', {'fields': [ 'description', 'duedate', 'location', 'completed', 'priority' ] }),
     ]
 
-    list_display = ('title', 'duedate', 'completed')
+    list_display = ('title', 'duedate', 'completed', 'priority')
     list_filter = ['duedate']
     search_fields = ['title']
 
