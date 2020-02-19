309aab114de439f712655bec622544d7ce76b0cb
Ann Hoang <lph2yr@virginia.edu>
2020-02-19 03:28:40 -0500

    fixed update view but why is it going to todoitem_form.html?

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
-from .forms import EditToDoForm
+#from .forms import EditToDoForm
 from .models import ToDoItem
 from django.views.generic.edit import CreateView, UpdateView
 
@@ -22,13 +22,16 @@ class AddToDoItemView(CreateView):
     model = ToDoItem
     fields = ['title', 'description', 'duedate', 'location']
 
-'''
+
 #function processes input data of Date and Time and updates it in Database for todo_item at todo_item_id
 class EditToDo(UpdateView):
     model = ToDoItem
-    fields = ['duedate']
-'''
+    fields = ['title', 'description', 'duedate', 'recur_freq', 'end_recur_date'] #new fields don't create new obj yet!!!
+    def form_valid(self, form):
+        form.instance.todo_item = self.request.user
+        return super().form_valid(form)
 
+'''
 #@param: request, todo_item_id
 def detail( request, todo_item_id ):
     todo_item = ToDoItem.objects.get(id=todo_item_id) #get current todo_item with the id
@@ -43,7 +46,7 @@ def detail( request, todo_item_id ):
         form = EditToDoForm() #reset blank form
     context = {'todo_item': todo_item, 'form':form}
     return render(request, 'todo/detail.html', context)
-
+'''
 #function changes a todo from incomplete to complete (completed = False -> True)
 def completeToDo(request, todo_item_id):
     #Todo item to be completed
