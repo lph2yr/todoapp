18bb23cb6333e4129c52483fb4261de640883d6d
Ann Hoang <lph2yr@virginia.edu>
2020-02-20 15:20:00 -0500

    changed some comments

--- todo/views.py
+++ todo/views.py
@@ -28,9 +28,6 @@ class EditToDo(UpdateView):
     model = ToDoItem
     fields = ['title', 'description', 'duedate', 'recur_freq', 'end_recur_date']
     #new fields (recur_freq, end_recur_date) don't create new obj yet!!!
-    def form_valid(self, form):
-        form.instance.todo_item = self.request.user
-        return super().form_valid(form)
 
 '''
 #@param: request, todo_item_id
