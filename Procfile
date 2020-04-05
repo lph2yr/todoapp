8348a513fc4774de5acf0c5e086f82e186404ac5
Andrew Wang <awang1030@gmail.com>
2020-04-05 16:22:54 -0400

    Changed Task Decorator

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 release: python manage.py migrate
 worker: celery -A personaldashboard worker --pool=solo --without-mingle -l info
+worker: celery -A personaldashboard beat -l info
 web: gunicorn personaldashboard.wsgi
--- todo/tasks.py
+++ todo/tasks.py
@@ -1,5 +1,5 @@
-from celery import shared_task
 from celery.schedules import crontab
+from .celery import app
 
 from django.utils import timezone
 import datetime
@@ -10,19 +10,19 @@ from django.core.mail import send_mail
 from .models import ToDoItem
 from django.contrib.auth.models import User
 
-@shared_task
-def check_for_notifications(user_id):
+@app.task
+def check_for_notifications():
     notify_interval_begin = datetime.now() + relativedelta(hours=+1)
     notify_interval_end = datetime.now() + relativedelta(hours=+1, minutes=+5)
-    todo_list = ToDoItem.objects.filter(user=user_id).filter(notify=true).filter(duedate__gte=notify_interval_begin).filter(duedate__lte=notify_interval_end)
+    todo_list = ToDoItem.objects.filter(notify=true).filter(duedate__gte=notify_interval_begin).filter(duedate__lte=notify_interval_end)
     for todo in todo_list:
         notify_time = todo.duedate + relativedelta(hours=-1)
-        queue_email.apply_async((todo_notify.id, user_id), eta=notify_time)
+        queue_email.apply_async((todo_notify.id), eta=notify_time)
 
-@shared_task
-def queue_email(todo_item_id, user_id):
+@app.task
+def queue_email(todo_item_id):
     todo_notify = ToDoItem.objects.get(id=todo_item_id)
-    user = User.objects.get(id=user_id)
+    user_to_notify = User.objects.get(id=todo_notify.user)
     due_date_local = todo_notify.duedate.astimezone(pytz.timezone("America/New_York"))
     send_mail(
         todo_notify.title + ' due soon!',
@@ -31,5 +31,5 @@ def queue_email(todo_item_id, user_id):
         '\n Description: \t' + todo_notify.description +
         '\n Due: \t' + due_date_local.strftime("%m/%d/%Y, %I:%M:%S %p"),
         'personaldashboard.bogosorters@gmail.com',
-        [user.email],
+        [user_to_notify.email],
    )
